e26b5fdee40b83b5be8a865f8c32f4aad5a46b2f
VictorUBB <96655025+VictorUBB@users.noreply.github.com>
2023-07-27 13:11:25 +0300

    First commit

---
+++ src/routes/createEntitys/CreateUser.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const CreateUser = () => {
+    return (
+        <div className="header">
+        <h1>Waiting Profiles</h1>
+        </div>
+    );
+    };
+
+export default WaitingProfiles;
